5283a3b48d4cfc548ed811d852ea48c8981cac8d
juabiDev <Mederos40@outlook.com>
2023-12-26 09:18:14 -0300

    Lazy Loading de las rutas, Hacer un i18n con las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,33 @@
+import { Suspense, lazy } from 'react'
+
 import './App.css'
-import HomePage from './pages/Home'
-import AboutPage from './pages/About'
 import Page404 from './pages/404'
 import SearchPage from './pages/SearchPage'
 
 import { Router } from './Router.jsx'
 import { Route } from './Route.jsx'
 
-const routes = [
+const LazyAboutPage = lazy(() => import('./pages/About.jsx'))
+const LazyHomePage = lazy(() => import('./pages/Home.jsx'))
+
+const appRoutes = [
   {
-    path: "/",
-    Component: HomePage
-  },
-  {
-    path: "/about",
-    Component: AboutPage
-  },
-  {
-    path: "/search/:query",
-    Component: SearchPage
+    path: '/:lang/about',
+    Component: LazyAboutPage,
   }
 ]
-
 
 
 function App() {
 
   return (
     <main>
-      <Router routes={routes} defaultComponent={Page404}>
-        <Route path="/" Component={HomePage} />
-        <Route path="/about" Component={AboutPage} />
-        <Route path="/search/:query" Component={SearchPage} />
-      </Router>
+      <Suspense fallback={null}>
+        <Router routes={appRoutes} defaultComponent={Page404}>
+          <Route path="/" Component={LazyHomePage} />
+          <Route path="/search/:query" Component={SearchPage} />
+        </Router>
+      </Suspense>
     </main>
   )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,33 @@
 import { Link } from "../Link";
 
-export default function AboutPage() {
+const i18n = {
+  es: {
+    title: "Sobre nosotros",
+    content: "¡Hola! Me llamo Fabián Mederos y estoy creando un clon de React Router.",
+    button: "Ir a la Home"
+  },
+  en: {
+    title: "About us",
+    content: "Hi! My name is Fabián Mederos and I'm creating a React Router clone.",
+    button: "Go to Home"
+  }
+}
+
+const useI18n = (lang) => {
+  return i18n[lang] || i18n.en
+}
+
+export default function AboutPage({ routeParams }) {
+  const i18n = useI18n(routeParams.lang ?? "es")
+
     return (
       <>
-        <h1>About</h1>
+        <h1>{i18n.title}</h1>
         <div>
           <img src="https://img.freepik.com/foto-gratis/lindo-gato-relajante-estudio_23-2150692717.jpg?size=626&ext=jpg&ga=GA1.1.1687694167.1703116800&semt=sph" alt="Foto de Fabián Mederos" />
-          <p>¡Hola! Me llamo Fabián Mederos y estoy creando un clon de React Router.</p>
+          <p>{i18n.content}</p>
         </div>
-        <Link to={"/"}>Ir a la Home</Link>
+        <Link to={"/"}>{i18n.button}</Link>
 
       </>
     )
